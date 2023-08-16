b9846044e9b3f385f246eab8c571c9c07a4369f6
annajang <annajang1234@gmail.com>
2023-08-16 09:39:56 -0400

    Initial Commit

---
+++ sustaini/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  sustaini
+//
+//  Created by scholar on 8/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sustaini/sustainiApp.swift
@@ -0,0 +1,17 @@
+//
+//  sustainiApp.swift
+//  sustaini
+//
+//  Created by scholar on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct sustainiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
